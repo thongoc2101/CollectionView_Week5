126966562100ceb42c466005693d02c850af3aec
Cntt04 <Admin04@Cntt04s-iMac.local>
2017-04-26 14:51:41 +0700

    Initial Commit

---
+++ Demoweek5/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  Demoweek5
+//
+//  Created by Cntt04 on 4/26/17.
+//  Copyright © 2017 Cntt04. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var MyImageView: UIImageView!
+}
